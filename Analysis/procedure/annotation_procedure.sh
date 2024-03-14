3df3c068bfa8725a6b1ff3bc523d2cc03d7d4b71
zaneveld <zaneveld@gmail.com>
2024-03-14 13:26:32 -0700

    Added outline for annotation_procedure.sh

---
+++ Analysis/procedure/annotation_procedure.sh
@@ -0,0 +1,20 @@
+#Calculate BUSCO scores
+
+#Run TransDecoder
+
+#Pick longest ORF 
+
+#CD-HIT clustering (90% similarity)
+
+#HMMR - Preprocess PFAM domains
+
+#HMMR - Search TransDecoder for PFAM domains
+
+#SignalPeptide 4.1 - annotate signal peptides
+
+#TMMHMMv2 - annotate transmembrane domains
+
+#python - Annotate TLR, IL-1R 
+
+
+
