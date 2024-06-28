c33a7ac3ad1c68cee431a52429d56e8d50534920
Jesse Zaneveld <zaneveld@gmail.com>
2024-06-28 11:07:39 -0700

    Update annotation_procedure.sh

--- Analysis/procedure/annotation_procedure.sh
+++ Analysis/procedure/annotation_procedure.sh
@@ -1,6 +1,12 @@
 #Calculate BUSCO scores
 
-#Run TransDecoder
+#Run RepeatModeler across genomes
+
+
+#Run RepeatMasker across genomes
+
+#Annotate genes with Braker3
+## Braker3 will re-calculate BUSCO scores (to check for missed ORFs)
 
 #Pick longest ORF 
 
@@ -8,7 +14,7 @@
 
 #HMMR - Preprocess PFAM domains
 
-#HMMR - Search TransDecoder for PFAM domains
+#HMMR - Search gene calls from Braker3 for PFAM domains
 
 #SignalPeptide 4.1 - annotate signal peptides
 
@@ -16,5 +22,6 @@
 
 #python - Annotate TLR, IL-1R 
 
+#python/R - Compare TLR, IL-1R counts vs. microbiome
 
 
